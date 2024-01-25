b300742c6e6853621f21dcac6d3c7a3aed010fa1
Juanda <heyjuns@icloud.com>
2024-01-25 11:01:24 +0700

    init source

---
+++ lib/timer/bloc/timer_bloc.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:bloc_timer/ticker.dart';
+import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+part 'timer_event.dart';
+part 'timer_state.dart';
+
+class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  static const int _duration = 60;
+  final Ticker _ticker;
+  StreamSubscription<int>? _tickerSubscription;
+
+  TimerBloc({required Ticker ticker})
+      : _ticker = ticker,
+        super(const TimerInitial(_duration)) {
+    on<TimerStarted>(_onStarted);
+    on<_TimerTicked>(_onTicked);
+    on<TimerPaused>(_onPaused);
+    on<TimerResumed>(_onResumed);
+    on<TimerReset>(_onReset);
+  }
+
+  FutureOr<void> _onResumed(event, emit) {
+    if (state is TimerPaused) {
+      _tickerSubscription?.resume();
+      emit(TimerRunInProgress(state.duration));
+    }
+  }
+
+  FutureOr<void> _onReset(event, emit) {
+    _tickerSubscription?.cancel();
+    emit(const TimerInitial(_duration));
+  }
+
+  FutureOr<void> _onPaused(event, emit) {
+    if (state is TimerRunInProgress) {
+      _tickerSubscription?.pause();
+      emit(TimerRunPause(state.duration));
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+
+  void _onStarted(TimerStarted event, Emitter<TimerState> emit) {
+    emit(TimerRunInProgress(event.duration));
+    _tickerSubscription?.cancel();
+    _tickerSubscription =
+        _ticker.tick(ticks: event.duration).listen((duration) {
+      add(_TimerTicked(duration: duration));
+    });
+  }
+
+  void _onTicked(_TimerTicked event, Emitter<TimerState> emit) {
+    emit(event.duration > 0
+        ? TimerRunInProgress(event.duration)
+        : const TimerRunComplete());
+  }
+}
